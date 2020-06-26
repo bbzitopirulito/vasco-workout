32850a7a6940cb14901a22553c5b63f7ac8e2008
Bernardo Mendes de Lima <bernardomendeslima99@gmail.com>
2020-06-27 00:20:00 +0100

    feat: starting to create workout groups

--- frontend/src/pages/admin-schedule/index.jsx
+++ frontend/src/pages/admin-schedule/index.jsx
@@ -8,6 +8,11 @@ import {
   Container,
   Form,
   FormGroup,
+  UncontrolledCollapse,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
 } from "reactstrap";
 import api from "../../services/api.js";
 import Dropdown from "react-bootstrap/Dropdown";
@@ -18,6 +23,20 @@ const AdminSchedule = () => {
   const [workouts, setWorkouts] = useState([]);
   const [limit, setLimit] = useState(6);
   const [type, setType] = useState("");
+  const [day, setDay] = useState(1);
+  const [newGroup, setNewGroup] = useState([]);
+
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+
+  const createGroup = () => {};
 
   const createSchedule = async () => {
     await api
@@ -57,63 +76,131 @@ const AdminSchedule = () => {
       <Menu />
       <div className="schedule min-vh-100 mt-5 d-flex flex-column align-items-baseline">
         <Container className="admin-container table-responsive">
-          <Table className="border border-secondary rounded">
-            <thead>
-              <tr>
-                <th>Dia</th>
-                <th>Hora</th>
-                <th>Pessoas</th>
-                <th>Limite</th>
-                <th>Tipo</th>
-                <th>Mudar</th>
-              </tr>
-            </thead>
-            <tbody>
-              {workouts.length > 0 &&
-                workouts.map((item, k) => (
-                  <tr id={k}>
-                    <td>
-                      {new Date(item.date).getDate()}/
-                      {new Date(item.date).getMonth()}/
-                      {new Date(item.date).getFullYear()}
-                    </td>
-                    <td>{new Date(item.date).getHours()}</td>
-                    <td>
-                      <Dropdown>
-                        <Dropdown.Toggle id="dropdown-basic">
-                          Pessoas
-                        </Dropdown.Toggle>
+          <Button color="primary my-3" id="toggler">
+            Workouts
+          </Button>
+          <UncontrolledCollapse toggler="#toggler">
+            <Table className="border border-secondary rounded">
+              <thead>
+                <tr>
+                  <th>Dia</th>
+                  <th>Hora</th>
+                  <th>Pessoas</th>
+                  <th>Limite</th>
+                  <th>Tipo</th>
+                  <th>Mudar</th>
+                </tr>
+              </thead>
+              <tbody>
+                {workouts.length > 0 &&
+                  workouts.map((item, k) => (
+                    <tr id={k}>
+                      <td>
+                        {new Date(item.date).getDate()}/
+                        {new Date(item.date).getMonth()}/
+                        {new Date(item.date).getFullYear()}
+                      </td>
+                      <td>{new Date(item.date).getHours()}</td>
+                      <td>
+                        <Dropdown>
+                          <Dropdown.Toggle id="dropdown-basic">
+                            Pessoas
+                          </Dropdown.Toggle>
 
-                        <Dropdown.Menu>
-                          {item.users.length > 0 ? (
-                            item.users.map((user, k) => (
-                              <Dropdown.Item id={k}>
-                                {user.username}
-                              </Dropdown.Item>
-                            ))
-                          ) : (
-                            <Dropdown.Item>Aula vazia</Dropdown.Item>
-                          )}
-                        </Dropdown.Menu>
-                      </Dropdown>
-                    </td>
-                    <td>{item.limit}</td>
-                    <td>{item.type}</td>
-                    <td>
-                      <Button
-                        color="danger"
-                        onClick={() => cancelWorkout(item._id)}
-                      >
-                        Cancelar
-                      </Button>
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </Table>
+                          <Dropdown.Menu>
+                            {item.users.length > 0 ? (
+                              item.users.map((user, k) => (
+                                <Dropdown.Item id={k}>
+                                  {user.username}
+                                </Dropdown.Item>
+                              ))
+                            ) : (
+                              <Dropdown.Item>Aula vazia</Dropdown.Item>
+                            )}
+                          </Dropdown.Menu>
+                        </Dropdown>
+                      </td>
+                      <td>{item.limit}</td>
+                      <td>{item.type}</td>
+                      <td>
+                        <Button
+                          color="danger"
+                          onClick={() => cancelWorkout(item._id)}
+                        >
+                          Cancelar
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </Table>
+          </UncontrolledCollapse>
+          {newGroup.length > 0 && (
+            <Table className="border border-secondary rounded">
+              <thead>
+                <tr>
+                  <th>Dia</th>
+                  <th>Hora</th>
+                  <th>Limite</th>
+                  <th>Tipo</th>
+                  <th>Mudar</th>
+                </tr>
+              </thead>
+              <tbody>
+                {newGroup.length > 0 &&
+                  newGroup.map((item, k) => (
+                    <tr id={k}>
+                      <td>
+                        {new Date(item.date).getDate()}/
+                        {new Date(item.date).getMonth()}/
+                        {new Date(item.date).getFullYear()}
+                      </td>
+                      <td>{new Date(item.date).getHours()}</td>
+                      <td>{item.limit}</td>
+                      <td>{item.type}</td>
+                      <td>
+                        <Button
+                          color="danger"
+                          onClick={() =>
+                            setNewGroup(newGroup.filter((f) => f.i !== item.i))
+                          }
+                        >
+                          Remover
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </Table>
+          )}
           <Form>
             <FormGroup>
-              <DateTimePicker onChange={(date) => setDate(date)} value={date} />
+              {newGroup ? (
+                <UncontrolledDropdown>
+                  <DropdownToggle caret>{weekDays[day]}</DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem onClick={() => setDay(1)}>
+                      Segunda
+                    </DropdownItem>
+                    <DropdownItem onClick={() => setDay(2)}>Terça</DropdownItem>
+                    <DropdownItem onClick={() => setDay(3)}>
+                      Quarta
+                    </DropdownItem>
+                    <DropdownItem onClick={() => setDay(4)}>
+                      Quinta
+                    </DropdownItem>
+                    <DropdownItem onClick={() => setDay(5)}>Sexta</DropdownItem>
+                    <DropdownItem onClick={() => setDay(6)}>
+                      Sábado
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              ) : (
+                <DateTimePicker
+                  onChange={(date) => setDate(date)}
+                  value={date}
+                />
+              )}
             </FormGroup>
             <FormGroup>
               <Label for="password">Limite de pessoas</Label>
@@ -123,9 +210,28 @@ const AdminSchedule = () => {
               <Label for="type">Tipo de aula</Label>
               <Input onChange={(e) => setType(e.target.value)} value={type} />
             </FormGroup>
-            <Button color="primary" onClick={() => createSchedule()}>
-              Criar
-            </Button>
+            <FormGroup>
+              <Button
+                color="primary"
+                onClick={() =>
+                  newGroup.length > 0 ? createGroup() : createSchedule()
+                }
+              >
+                {newGroup.length > 0 ? "Criar grupo" : "Criar"}
+              </Button>
+              <Button
+                className="ml-2"
+                color="primary"
+                onClick={() =>
+                  setNewGroup([
+                    ...newGroup,
+                    { date, type, limit, i: new Date().getMilliseconds() },
+                  ])
+                }
+              >
+                Adicionar grupo
+              </Button>
+            </FormGroup>
           </Form>
         </Container>
       </div>
